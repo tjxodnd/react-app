186de9fd25708ac92320c60418e9030de084680f
stw <eru962naver.com>
2021-06-15 16:54:13 +0900

    최초커밋

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import MyReactApp from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MyReactApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
